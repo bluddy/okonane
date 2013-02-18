8427fe6852e105461386c368b425317e92981427
Yotam Barnoy <yotambarnoy@gmail.com>
2013-02-18 14:36:01 -0500

    Scaffolding near done. Some exception on 3rd turn

--- hw2/barnoy.yotam/bin/Main.ml
+++ hw2/barnoy.yotam/bin/Main.ml
@@ -2,6 +2,18 @@ open Util
 open Board
 open Game
 
-let main () = start_game ()
+let debug = ref false
 
+let param_specs = Arg.align 
+    [
+        "-d", Arg.Set  debug, " Show debug information";
+    ]
+
+let parse_cmd_line () = Arg.parse param_specs (fun str -> ()) ""
+
+let main () = 
+  parse_cmd_line ();
+  start_game !debug
+
+let _ = main ()
 
