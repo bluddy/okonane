b906c5021811a6dd5700247559f6f34240b1ee4b
Yotam Barnoy <yotambarnoy@gmail.com>
2013-04-19 15:53:38 -0400

    Modified runall

--- hw4/barnoy.yotam/runall.rb
+++ hw4/barnoy.yotam/runall.rb
@@ -3,16 +3,22 @@
 require 'optparse'
 require 'pathname'
 
-files = ["house-votes-84.data", "monks-1", "monks-2", "monks-3", "mushroom", "splice"]
-a = ARGV.drop 1
-type = a.take 1
-args = a.drop 1
+files = ["house-votes-84", "monks-1", "monks-2", "monks-3", "mushroom", "splice"]
+print "#{ARGV}\n"
+type = ARGV.take 1
+args = ARGV.drop 1
+
+args_str = ""
+for s in args
+    args_str = args_str + " " + s
+end
+    
 
 files.each {|f|
     path = File.join("./data/","#{f}.data")
     outfile = File.join("./output/", "#{f}.#{type}")
-    str = "run#{type}.sh #{path} #{args} > outfile 2>&1"
-    print str
+    str = "./run#{type}.sh #{path} #{args_str} > #{outfile} 2>&1"
+    print "#{str}\n"
     `#{str}`
 }
 
